cdc209d014720167894209288d055026466c286d
Steve Usher <steveush@gmail.com>
2014-01-08 20:57:24 +0200

    Refactoring, commenting and some minor changes.

--- js/foonav.js
+++ js/foonav.js
@@ -2,54 +2,107 @@
 
 	window.FooNav = {
 		defaults: {
+			/** @type {(string|HTMLElement|Array|jQuery)} - A DOM element, array of elements, HTML string, or jQuery object to insert after the generated menu. */
 			after: null,
+			/** @type {(string|HTMLElement|Array|jQuery)} - A DOM element, array of elements, HTML string, or jQuery object to insert before the generated menu. */
 			before: null,
+			/** @type {string} - A string of space separated class names to add to the navigation element. */
 			classes: null,
-			home: null,
+			/** @type {boolean} - Whether or not to display the 'To Top' button. */
 			top: true,
+			/** @type {string} - The class name of the position for the navigation element. */
 			position: 'fon-bottom-right',
+			/** @type {string} - The class name of the theme for the navigation element. */
 			theme: 'fon-light',
+			/** @type {number} - The distance the scroll bars must travel before displaying the navigation element. */
 			scroll: 0,
+			/** @type {number} - The speed the navigation element is shown/hidden and the speed the menus are transitioned between. */
 			speed: 200,
+			/** @namespace - Contains all the 'smart' options. */
 			smart: {
+				/** @type {boolean} - Whether or not to enable the smart options. This disables all smart options if set to false. */
 				enable: true,
+				/** @type {boolean} - Whether or not to track anchors in the page. If set to true the menu will automatically find the item corresponding to the current visible anchor. */
 				anchors: true,
+				/** @type {boolean} - Whether or not to close the menu when losing focus. If set to true when a user clicks anywhere on the page that is not within a navigation element, this instance will close. */
 				close: true,
+				/** @type {boolean} - Whether or not to auto open the navigation element on page load. If set to true and the page is scrolled past the scroll option value and a tracked anchor is visible the navigation element will be displayed. */
 				open: true,
+				/** @type {boolean} - Whether or not to remember menu position on toggle. If set to true the menu will remember it's current position while being toggled. If set to false when the menu is displayed or redisplayed it is reset to the root. */
 				remember: true,
+				/** @type {boolean} - Whether or not to enable smart scrolling. If set to true and a user clicks on an anchored item the page will smoothly scroll to the anchor from it's current position. */
 				scroll: true,
+				/** @type {boolean} - Whether or not to parse the current url for a hash value. If a hash is found and it matches an item the menu is set to display that item. */
 				url: true
 			},
+			/** @type {string} - A string to display above the root menu items. This is replaced by the back button text on child menus. */
 			title: 'FooNav',
+			/** @type {string} - A string specifying the type of transition to use on the menu. Possible values are 'slide' and 'fade' */
 			transition: 'slide',
+			/** @namespace - Contains all the icon class information. */
 			icons: {
-				set: null,
-				back: 'fon-icon-back',
-				expand: 'fon-icon-expand',
-				home: 'fon-icon-home',
-				menu: 'fon-icon-menu',
-				top: 'fon-icon-top'
+				back: { family: 'fon-icon', icon: 'fon-icon-back' },
+				expand: { family: 'fon-icon', icon: 'fon-icon-expand' },
+				menu: { family: 'fon-icon', icon: 'fon-icon-menu' },
+				top: { family: 'fon-icon', icon: 'fon-icon-top' }
 			},
+			/**
+			 * A PlainObject containing any additional buttons to create, these can be either just the href or
+			 * an object of attributes, events, and methods to call on the newly-created button.
+			 * The name used must match an icon defined in the icons object.
+			 * @example <caption>A string</caption>
+		 	 * buttons: {
+			 * 	[name]: [string]
+			 * }
+			 * @example <caption>An object of attributes, events, and methods.</caption>
+			 * buttons: {
+			 * 	[name]: [object]
+			 * }
+			 * @see {@link http://api.jquery.com/jQuery/#jQuery-html-attributes|jQueryAPI} for more information on the html attributes object.
+			 */
+			buttons: null,
+			/**
+			 * An array of PlainObjects defining the items to display in the menu.
+			 * @example <caption>Example item object</caption>
+			 * items: [{
+			 * 	href: [string],
+			 * 	text: [string],
+			 * 	children: [array]
+			 * }]
+			 */
 			items: []
 		},
+		/**
+		 * Contains all instantiated instances of FooNav.
+		 */
 		instances: []
 	};
 
-	FooNav.get = function(index){
+	/**
+	 * Retrieves an instance of FooNav by index. Indexes are assigned from zero in the order they were created.
+	 * @param {number} index - The index of the instance to retrieve.
+	 * @returns {FooNav.Instance}
+	 */
+	FooNav.fetch = function(index){
 		index = index > FooNav.instances.length - 1 ? FooNav.instances.length - 1 : index < 0 ? 0 : index;
 		return FooNav.instances[index];
 	};
 
-	FooNav.create = function(options){
+	/**
+	 * Initializes a new instance of FooNav with the given options.
+	 * @param {object} options - The options to initialize FooNav with.
+	 * @returns {FooNav.Instance}
+	 */
+	FooNav.init = function(options){
 		return new FooNav.Instance(options);
 	};
 
 	FooNav.reinit = function(index, options){
-		FooNav.get(index).reinit(options);
+		FooNav.fetch(index).reinit(options);
 	};
 
 	FooNav.destroy = function(index){
-		FooNav.get(index).destroy();
+		FooNav.fetch(index).destroy();
 	};
 
 	FooNav.Timer = function(){
@@ -75,17 +128,16 @@
 		this.o = $.extend(true, {}, FooNav.defaults);
 		this.nav = null;
 		this.inner = null;
+		this.buttons = null;
 		this.back = null;
 		this.top = null;
 		this.toggle = null;
-		this.home = null;
 		this.root = null;
 
+		/** @type {FooNav.Instance} - Hold a reference to this object to use within functions to avoid scoping issues. */
 		var _ = this,
-			noop = function(){},
-			kill = function(e){
-				e.preventDefault();e.stopPropagation();
-			};
+			/** @type {function} - An empty function used for to perform no operation. */
+			noop = function(){};
 
 		this.init = function(o){
 			_.o = $.extend(true, _.o, o);
@@ -118,7 +170,7 @@
 			partial = partial || false;
 			_.nav.remove();
 			_.o = $.extend(true, {}, FooNav.defaults);
-			_.nav = _.inner = _.back = _.top = _.toggle = _.home = _.root = null;
+			_.nav = _.inner = _.back = _.top = _.toggle = _.root = null;
 			$(window)
 				.off('scroll', _.a.check)
 				.off('scroll', _.w.scrolled)
@@ -128,21 +180,52 @@
 			}
 		};
 
+		/** @namespace - Contains all util type functions. */
 		this.u = {
+			/** @type {HTMLAnchorElement} - An internal anchor element used to fully qualify urls. */
 			_a: document.createElement('a'),
+			/**
+			 * Turns a partial url into a fully qualified one.
+			 * @param {string} url - A partial url.
+			 * @returns {string}
+			 */
 			qualify: function(url){
 				_.u._a.href = url;
 				return _.u._a.href;
 			},
-			classes: function(className, classNameN){
-				var a = [], i;
-				for (i = 0; i < arguments.length; i++){ a.push(arguments[i]); }
-				return a.join(' ');
+			/**
+			 * Checks if the supplied url is the current page url.
+			 * @param {string} url - The url to check.
+			 * @returns {boolean}
+			 */
+			isCurrent: function(url){
+				return _.u.qualify(url) == [location.protocol, '//', location.host, location.pathname].join('');
 			},
+			/**
+			 * Concatenates the supplied string arguments into a single space delimited string.
+			 * @param {string} arg1 - The first string.
+			 * @param {string} arg2 - A second string to concatenate with the first.
+			 * @param {string} [argN] - Additional strings to concatenate.
+			 * @returns {string}
+			 */
+			concat: function(arg1, arg2, argN){
+				return Array.prototype.slice.call(arguments).join(' ');
+			},
+			/**
+			 * Returns an object containing the vertical and horizontal position of the menu.
+			 * @returns {{v: string, h: string}}
+			 */
 			position: function(){
 				var p = _.o.position.split('-');
 				return { v: p[1], h: p[2] };
 			},
+			/**
+			 * Checks if the supplied href exists in the page (i.e. starts with a # and it exists).
+			 * @param {string} href - The href to check.
+			 * @param {function} [yes] - The function to execute if the href exists.
+			 * @param {function} [no] - The function to execute if the href does NOT exist.
+			 * @returns {boolean}
+			 */
 			exists: function(href, yes, no){
 				yes = yes || noop;
 				no = no || noop;
@@ -161,86 +244,128 @@
 			}
 		};
 
+		/** @namespace - Contains all the functions used to build the menu. */
 		this.b = {
+			/**
+			 * Create the outer container for the plugin.
+			 */
 			nav: function() {
-				_.nav = $('<div/>', {
-					'class': _.u.classes('fon-nav', _.o.position, _.o.theme, _.o.icons.set, _.o.classes),
+				_.nav = $('<div></div>', {
+					'class': _.u.concat('fon-nav', _.o.position, _.o.theme, _.o.classes),
 					css: { display: 'none' }
 				})
 					.on('click', '.fon-item-link', _.m.clicked)
 					.on('click', '.fon-item-back', _.m.back)
 					.appendTo('body');
-
-				_.inner = $('<div/>', { 'class': 'fon-nav-inner' }).appendTo(_.nav);
 			},
+			/**
+			 * Create the button container and the buttons within it.
+			 */
 			buttons: function(){
-				_.top = $('<a/>', {
-					'class': 'fon-button',
-					href: '#top',
-					css: { display: 'none' },
-					on: {	click: _.w.top }
-				}).append($('<span/>', { 'class': _.u.classes('fon-icon', _.o.icons.top) }));
+				_.buttons = $('<div></div>', { 'class': 'fon-buttons' }).appendTo(_.nav);
 
-				_.toggle = $('<a/>', {
-					'class': 'fon-button',
-					href: '#toggle',
-					on: {	click: _.m.toggle }
-				}).append($('<span/>', { 'class': _.u.classes('fon-icon', _.o.icons.menu) }));
-
-				_.home = $('<a/>', {
-					'class': 'fon-button',
-					href: _.o.home
-				}).append($('<span/>', { 'class': _.u.classes('fon-icon', _.o.icons.home) }));
-
-				if (typeof _.o.home != 'string' || (typeof _.o.home == 'string' && _.u.qualify(_.o.home) == [location.protocol, '//', location.host, location.pathname].join(''))){
-					_.home.css('display','none');
+				function _icon(name){
+					var i = _.o.icons[name];
+					if (!i) return $();
+					return $('<span></span>', { 'class': _.u.concat('fon-button-icon', i.family, i.icon) });
 				}
 
-				$('<div/>', {
-					'class': 'fon-buttons'
-				}).append(_.top, _.toggle, _.home).appendTo(_.nav);
-			},
-			_menu: function(menu, parent, items){
-				var i, l = items.length, $menu, $li, $item, item;
-				if (parent != null){
-					$li = $('<li/>').appendTo(menu);
-					$item = $('<a/>', { 'class': 'fon-item fon-item-back', href: parent.href, text: parent.text })
-						.prepend($('<span/>', { 'class': _.u.classes('fon-icon', _.o.icons.back) }))
-						.appendTo($li);
+				// the top and toggle buttons have extra functionality so hold a reference to them for later use
+				_.top = $('<a></a>', { 'class': 'fon-button', href: '#top', css: { display: 'none' }, on: { click: _.w.top }})
+					.append(_icon('top'))
+					.appendTo(_.buttons);
 
-					_.u.exists(parent.href, function(){
-						$item.addClass('fon-anchored');
-					});
-				} else if (typeof _.o.title == 'string'){
-					$li = $('<li/>').appendTo(menu);
-					$item = $('<span/>', { 'class': 'fon-item fon-item-title fon-anchored', text: _.o.title })
-						.appendTo($li);
-				}
-				for (i = 0; i < l; i++){
-					item = items[i];
-					$li = $('<li/>').appendTo(menu);
-					$item = $('<a/>', { 'class': 'fon-item fon-item-link', href: item.href, text: item.text }).appendTo($li);
-					_.u.exists(item.href, function(){
-						$item.addClass('fon-anchored');
-					});
-					if ($.isArray(item.children)){
-						$item.addClass('fon-has-menu').prepend($('<span/>', { 'class': 'fon-icon ' + _.o.icons.expand }));
-						$menu = $('<ul/>', { 'class': 'fon-menu' }).appendTo($li);
-						_.b._menu($menu, item, item.children);
+				_.toggle = $('<a></a>', { 'class': 'fon-button', href: '#toggle', on: { click: _.m.toggle }})
+					.append(_icon('menu'))
+					.appendTo(_.buttons);
+
+				if (typeof _.o.buttons == 'object') {
+					// if any additional buttons are defined iterate through them and create the elements.
+					for (var name in _.o.buttons){
+						// if this property is inherited or does NOT have a matching icon continue.
+						if (!_.o.buttons.hasOwnProperty(name) || !_.o.icons.hasOwnProperty(name)) continue;
+
+						var prop = _.o.buttons[name];
+						if (typeof prop === 'string'){ // if all that was supplied was a string (the href) build a button
+							// check for a home button, if the url of the home button and the current page are the same just don't create it.
+							if (name == 'home' && _.u.isCurrent(prop)) continue;
+
+							$('<a></a>', { 'class': 'fon-button', href: prop })
+								.append(_icon(name))
+								.appendTo(_.buttons);
+						} else if (typeof prop === 'object'){ // if the object initializer was supplied use it but append any required classes
+							// check for a home button, if the url of the home button and the current page are the same just don't create it.
+							if (typeof prop.href != 'string' || (name == 'home' && _.u.isCurrent(prop.href))) continue;
+							// fix up the class property adding in the required classes
+							prop['class'] = typeof prop['class'] === 'string' ? _.u.concat('fon-button', prop['class']) : 'fon-button';
+
+							$('<a></a>', prop)
+								.append(_icon(name))
+								.appendTo(_.buttons);
+						}
 					}
 				}
+				//adjust the minimum height to accommodate all the buttons
+				var bh = _.toggle.outerHeight() * _.buttons.children().length;
+				// adjust for button offset defined in '.fon-[top|bottom]-[left|right] > .fon-buttons' classes
+				var t = parseInt(_.buttons.css('top'), 0),
+					b = parseInt(_.buttons.css('bottom'), 0),
+					o = isNaN(t) ? (isNaN(b) ? 0 : b) : t;
+				_.nav.css('min-height', bh + o);
 			},
+			/**
+			 * Create the inner container and the menu within it.
+			 */
 			menu: function(){
-				_.root = $('<ul/>', { 'class': 'fon-menu' });
-				_.b._menu(_.root, null, _.o.items);
-				_.inner.empty().append(_.root.clone());
+				_.inner = $('<div></div>', { 'class': 'fon-nav-inner' }).appendTo(_.nav);
+				_.root = $('<ul></ul>', { 'class': 'fon-menu' });
+
+				function _icon(name){
+					var i = _.o.icons[name];
+					if (!i) return $();
+					return $('<span></span>', { 'class': _.u.concat('fon-item-icon', i.family, i.icon) });
+				}
+
+				// iterates through all the items and their children building the root ul
+				function _build(menu, parent, items){
+					var i, l = items.length, $menu, $li, $item, item;
+
+					if (parent != null){
+						$li = $('<li></li>').appendTo(menu);
+						$item = $('<a></a>', { 'class': 'fon-item fon-item-back', href: parent.href, text: parent.text })
+							.prepend(_icon('back'))
+							.appendTo($li);
+						if (_.u.exists(parent.href)) $item.addClass('fon-anchored');
+					} else if (typeof _.o.title == 'string'){
+						$li = $('<li></li>').appendTo(menu);
+						$item = $('<span></span>', { 'class': 'fon-item fon-item-title fon-anchored', text: _.o.title })
+							.appendTo($li);
+					}
+					for (i = 0; i < l; i++){
+						item = items[i];
+						$li = $('<li></li>').appendTo(menu);
+						$item = $('<a></a>', { 'class': 'fon-item fon-item-link', href: item.href, text: item.text })
+							.appendTo($li);
+						if (_.u.exists(item.href)) $item.addClass('fon-anchored');
+						if ($.isArray(item.children)){
+							$item.addClass('fon-has-menu').prepend(_icon('expand'));
+							$menu = $('<ul></ul>', { 'class': 'fon-menu' }).appendTo($li);
+							_build($menu, item, item.children);
+						}
+					}
+				}
+				_build(_.root, null, _.o.items);
+				_.inner.append(_.root.clone());
 			},
+			/**
+			 * Create the additional extras defined by the options.
+			 */
 			extra: function(){
 				if (typeof _.o.after == 'string'){
-					_.nav.append($('<div/>', { 'class': 'fon-after' }).html(_.o.after));
+					_.nav.append($('<div></div>', { 'class': 'fon-after' }).html(_.o.after));
 				}
 				if (typeof _.o.before == 'string'){
-					_.nav.prepend($('<div/>', { 'class': 'fon-before' }).html(_.o.before));
+					_.nav.prepend($('<div></div>', { 'class': 'fon-before' }).html(_.o.before));
 				}
 			}
 		};
@@ -285,10 +410,10 @@
 			size: function(menu){
 				var $nav = $('.fon-nav-size');
 				if ($nav.length == 0){
-					$nav = $('<div/>',{ 'class': 'fon-nav-size'	}).appendTo('body');
-					$('<div/>',{ 'class': 'fon-nav-inner' }).appendTo($nav);
+					$nav = $('<div></div>',{ 'class': 'fon-nav-size'	}).appendTo('body');
+					$('<div></div>',{ 'class': 'fon-nav-inner' }).appendTo($nav);
 				}
-				$nav.removeClass().addClass(_.u.classes('fon-nav-size', _.o.theme, _.o.icons.set, _.o.classes));
+				$nav.removeClass().addClass(_.u.concat('fon-nav-size', _.o.theme, _.o.classes));
 				var $inner = $nav.find('.fon-nav-inner').empty().append(menu.clone());
 				return {
 					height: $inner.height(),
